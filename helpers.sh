d241933be13ff0c0e87254f753f4dcaafe025e79
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2024-06-02 12:55:11 -0500

    add gdmf

--- helpers.sh
+++ helpers.sh
@@ -2,8 +2,22 @@
 cdc(){
   cd $(ls -d */ | fzf)
 }
+
+# Git Diff Main File
+# Description: This function will show a list of the files changed on Git since the last main branch commit. After selecting a file, it will show the diff of the file.
+# No parameters
 gdmf()
 {
-  git diff origin/main -- $(git diff main --name-only | fzf)
+  git fetch --prune
+  git diff origin/main -- $(git diff origin/main --name-only | fzf)
 }
 
+gcore()
+{
+  git fetch --prune
+  branche=$(git branch -r |\
+    sed 's/origin\///' |\
+    sed 's/^[[:space:]]*//' |\
+    fzf)
+  git checkout $branche
+}
